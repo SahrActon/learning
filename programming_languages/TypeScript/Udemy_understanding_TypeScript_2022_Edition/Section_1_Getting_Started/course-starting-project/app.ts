88949d169ec54a535baade4f4429c10ab3f68b15
Sahr Thomas Acton <tommy-acton@hotmail.co.uk>
2022-08-27 17:11:26 +0100

    Pushing the work that i have done today

--- programming_languages/TypeScript/Udemy_understanding_TypeScript_2022_Edition/Section_1_Getting_Started/course-starting-project/app.ts
+++ programming_languages/TypeScript/Udemy_understanding_TypeScript_2022_Edition/Section_1_Getting_Started/course-starting-project/app.ts
@@ -1,16 +1,29 @@
-const person: {
-  name: string; // this is a number
-  age: number; // this is a number
-  hobbies: string[]; // this is an array
-  role: [number, string]; // this marks a tuple - this is a special construct
-} = {
-  name: "Sahr", // 
+// const person: {
+//     name: string; // this is a number
+//     age: number; // this is a number
+//     hobbies: string[]; // this is an array
+//     role: [number, string]; // this marks a tuple - this is a special construct
+//   } = {
+//     name: "Sahr", //
+//     age: 28, // number
+//     hobbies: ["MMA", "Basketball"], // array
+//     role: [2, "author"], // tuple
+//   };
+
+enum Role {
+  ADMIN,
+  READY_ONLY,
+  AUTHOR,
+  STUDENT = "STUDENT",
+  ROOT = 0,
+}
+
+const person = {
+  name: "Sahr", //
   age: 28, // number
   hobbies: ["MMA", "Basketball"], // array
-  role: [2, "author"], // tuple
+  role: Role.ADMIN,
 };
-
-person.role = [0, 'admin']
 
 let favoriteActivities: string[];
 favoriteActivities = ["MMA", "Basketball"];
@@ -19,4 +32,8 @@ for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
 
-console.log(person.name);
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
+} else {
+  console.log("is not author");
+}
